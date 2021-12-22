841d09d0e84caa045aa76e4335478485a5804e3a
Riley Pearce <rilrom2@hotmail.com>
2021-12-22 13:20:35 +1030

    fix for no-anonymous-default-export warning

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   let vacancies = [
     {
       title: "Job",
